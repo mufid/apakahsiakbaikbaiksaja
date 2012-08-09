dad7155fcd2627383ca7a81828845f5dce5e2740
Mufid <mufid@idmail.or.id>
2012-08-09 23:58:06 +0700

    bismillah

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.listen(80, function(){
+app.set('port', process.env.PORT || 3007);
+app.listen(process.env.PORT || 3007, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
